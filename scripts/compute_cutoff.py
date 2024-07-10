5d5531e0c87d899d17b27006a46a0959450582fb
Zach Yarbrough <zach.yarbrough@ldas-pcdev6.ligo-la.caltech.edu>
2024-07-10 02:44:33 -0500

    added support for pycbc trigger files, and logic to handle multiple smaller input files, rather than one large one

--- scripts/compute_cutoff.py
+++ scripts/compute_cutoff.py
@@ -14,6 +14,7 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 
 parser = argparse.ArgumentParser()
+parser.add_argument("--pipeline", type=str)
 parser.add_argument("--pipeline-triggers", type=str, help="Path to raw pipeline triggers")
 parser.add_argument("--clean-triggers", type=str)
 parser.add_argument("--dirty-triggers", type=str)
@@ -59,15 +60,33 @@ machine_learning_samples = 1000
 cutoff_params = ['snr', 'chisqBysnrsq'] 
 machine_learning_cat = False 
 
+#raw_files = os.listdir(args.pipeline_triggers)
+#raw_triggers = pd.DataFrame()
+#for file in raw_files:
+#    df = pd.read_csv(os.path.join(args.pipeline_triggers, file))
+#    raw_triggers = pd.concat([raw_triggers, df], ignore_index=True)
 
-#print("Listing clean, dirty, and other trigger files")
-raw_files = args.pipeline_triggers 
-clean_files = args.clean_triggers 
-dirty_files = args.dirty_triggers 
-other_files = args.other_triggers 
+clean_files = [os.path.join(args.clean_triggers, file) for file in os.listdir(args.clean_triggers)]
+clean_triggers = pd.DataFrame()
+for file in clean_files:
+    df = pd.read_csv(os.path.join(args.clean_triggers, file))
+    clean_triggers = pd.concat([clean_triggers, df], ignore_index=True)
+
+dirty_files = [os.path.join(args.dirty_triggers, file) for file in os.listdir(args.dirty_triggers)]
+dirty_triggers = pd.DataFrame()
+for file in dirty_files:
+    df = pd.read_csv(os.path.join(args.dirty_triggers, file))
+    dirty_triggers = pd.concat([dirty_triggers, df], ignore_index=True)
+
+other_files = [os.path.join(args.other_triggers, file) for file in os.listdir(args.other_triggers)]
+other_triggers = pd.DataFrame()
+for file in other_files:
+    df = pd.read_csv(os.path.join(args.other_triggers, file))
+    other_triggers = pd.concat([other_triggers, df], ignore_index=True)
+
 # other_files = sorted([other_trigger_dir + f for f in os.listdir(other_trigger_dir) if f.endswith(".csv")])[:1]
-allFiles = [clean_files,  dirty_files,  other_files]
-print(f"All files: {allFiles}")
+allFiles = clean_files + dirty_files
+#print(f"All files: {allFiles}")
 
 if machine_learning_cutoff:
     print("Using Machine Learning for cutoff")
@@ -87,7 +106,7 @@ if machine_learning_cutoff:
     #    else:
     #        set_clean = np.append(set_clean, triggers[cutoff_params].sample(nClean).values, axis = 0)
     
-    triggers = pd.read_csv(clean_files, index_col = 0)
+    triggers = clean_triggers 
     for param in cutoff_params:
         if param not in triggers.columns:
             triggers[param] = formula(triggers, param)
@@ -100,27 +119,54 @@ if machine_learning_cutoff:
     scaled_clean = scaler.fit_transform(set_clean)
     print("Training SVM")
     clf = OneClassSVM(kernel="rbf", nu=0.1).fit(scaled_clean)
-        if args.save_model:
-            print("Saving model")
-            with open(f"{args.model_output_path}/trained_svm_classifier.pkl", 'wb') as fid:
-                pkl.dump(clf, fid)
-            print("Model saved")
-    print("Computing Cutoff Statistic for All files")
-    for file in allFiles:
+    if args.save_model:
+        print("Saving model")
+        with open(f"{args.model_output_path}/trained_svm_classifier.pkl", 'wb') as fid:
+            pkl.dump(clf, fid)
+        print("Model saved")
+
+    print("Computing Cutoff Statistic for Dirty files")
+
+    for file in dirty_files:
         print(file)
-        triggers = pd.read_csv(file, index_col = 0)
-        
+        triggers = pd.read_csv(file)
+        if len(triggers) == 0:
+            continue
         ifo_triggers = {}
-        for ifo in ['H1', 'L1']:
-            ifo_triggers[ifo] = triggers[triggers.ifo == ifo]
         
-            for param in cutoff_params:
-                if param not in ifo_triggers[ifo].columns:
-                    ifo_triggers[ifo][param] = formula(ifo_triggers[ifo], param)
-            data = ifo_triggers[ifo][cutoff_params].values
-            scaled_data = scaler.transform(data)
-            ifo_triggers[ifo]['vsv'] = -clf.decision_function(scaled_data)
-        new_df = pd.concat([ifo_triggers['H1'], ifo_triggers['L1']])
-        new_df.to_csv(file)    
-    
+        if args.pipeline == 'gstlal':
+            for ifo in ['H1', 'L1']:
+                ifo_triggers[ifo] = triggers[triggers.ifo == ifo]
+            
+                for param in cutoff_params:
+                    if param not in ifo_triggers[ifo].columns:
+                        ifo_triggers[ifo][param] = formula(ifo_triggers[ifo], param)
+                data = ifo_triggers[ifo][cutoff_params].values
+                scaled_data = scaler.transform(data)
+                ifo_triggers[ifo]['vsv'] = -clf.decision_function(scaled_data)
+            new_df = pd.concat([ifo_triggers['H1'], ifo_triggers['L1']])
+            new_df.to_csv(file)    
+
+        elif args.pipeline == 'pycbc':
+            hostname = os.getenv('HOSTNAME')
+            site = hostname.split('-')[2].split('.')[0]
+            if site == 'la':
+                site_ifo = 'L1'
+            elif site == 'wa':
+                site_ifo = 'H1'
+            else:
+                print('hostname not recognized')
+
+            for ifo in [site_ifo]:
+                ifo_triggers[ifo] = triggers[triggers.ifo == ifo]
+            
+                for param in cutoff_params:
+                    if param not in ifo_triggers[ifo].columns:
+                        ifo_triggers[ifo][param] = formula(ifo_triggers[ifo], param)
+                data = ifo_triggers[ifo][cutoff_params].values
+                scaled_data = scaler.transform(data)
+                ifo_triggers[ifo]['vsv'] = -clf.decision_function(scaled_data)
+            new_df = ifo_triggers[site_ifo]
+            new_df.to_csv(file)
+ 
 print("Done with compute_cutoff.py")
