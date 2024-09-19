8eb436a17417ad0d013887d20ba007b4246952da
Zach Yarbrough <zach.yarbrough@ligo-hd-01.gwave.ics.psu.edu>
2024-09-19 10:33:43 -0400

    update chunk_parse to handle csvs

--- scripts/chunk_parse.py
+++ scripts/chunk_parse.py
@@ -1,23 +1,34 @@
 #! /usr/bin/env/ python3
 
 import argparse
+import pandas as pd
 
 class ChunkParse: 
     def parse_chunk_file(self, chunk, chunk_definition_file):
         chunk = str(chunk)
+        
+        if chunk_definition_file.endswith('.txt'):
+            chunk_dict = {}
+            with open(chunk_definition_file, "r") as file:
+                lines = file.readlines()
 
-        chunk_dict = {}
-        with open(chunk_definition_file, "r") as file:
-            lines = file.readlines()
+                for line in lines:
+                    elements = line.split()
+                    chunk_dict[elements[0]] = [elements[1], elements[2]]
 
-            for line in lines:
-                elements = line.split()
-                chunk_dict[elements[0]] = [elements[1], elements[2]]
+            del chunk_dict['#']
 
-        del chunk_dict['#']
+            start = chunk_dict[chunk][0]
+            end = chunk_dict[chunk][1]
 
-        start = chunk_dict[chunk][0]
-        end = chunk_dict[chunk][1]
+        elif chunk_definition_file.endswith('.csv'):
+            print('reading csv')
+            df = pd.read_csv(chunk_definition_file, usecols=['chunk', 'start', 'end'], index_col=False)
+            #indiv_df = df[df['chunk'] == int(chunk)]
+            #start = indiv_df['start']
+            #end = indiv_df['end']
+            start = df.iloc[int(chunk)-1]['start']
+            end = df.iloc[int(chunk)-1]['end']
         return start, end
 
 if __name__ == '__main__':
